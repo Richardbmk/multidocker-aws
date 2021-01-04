99be9ae9df0919ac02edc3aac0223ecd77b76db6
Ricardo <rdobmk@gmail.com>
2021-01-04 22:57:07 +0100

    testing changes

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
-      Im some other page!
+      Holy glorious this is working on AWS!!
       <Link to="/">Go back home</Link>
     </div>
   );
